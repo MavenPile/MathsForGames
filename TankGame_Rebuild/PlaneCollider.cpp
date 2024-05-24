a8ae466e30618692cc8d2dab50e50ef5c6fdae1a
MavenPile <s214290@students.aie.edu.au>
2024-05-24 14:36:34 +0930

    PlaneCollider now works
    -enums are declared in header files, and are apart of the header, so you need to access it with a Class:: accessor
    -Also, Game() can accept a vector of textures, and main() can swtich betwen a vector or hardcoded initialisation. This wasn't mentioned in the previous commit

--- TankGame_Rebuild/PlaneCollider.cpp
+++ TankGame_Rebuild/PlaneCollider.cpp
@@ -1,114 +1,107 @@
-//#include "PlaneCollider.h"
-//#include "CircleCollider.h"
-//#include "AABBCollider.h"
-//
-//PlaneCollider::PlaneCollider(float x, float y, float offset)
-//{
-//	m_normal = Math::Vector3(x, y, 1);
-//	m_offset = offset;
-//}
-//
-//PlaneCollider::PlaneCollider(const Math::Vector3& normal, float offset)
-//{
-//	m_normal = normal;
-//	m_offset = offset;
-//}
-//
-//PlaneCollider::PlaneCollider(const Math::Vector3& p1, const Math::Vector3& p2)
-//{
-//	auto v = p2 - p1;
-//	v.Normalise();
-//
-//	m_normal.x = -v.y;
-//	m_normal.y = v.x;
-//
-//	m_offset = -p1.Dot(m_normal);
-//}
-//
-//PlaneCollider::PlaneCollider(const Math::Vector3& p1, const Math::Vector3& p2, const Math::Vector3& p3)
-//{
-//	auto v1 = (p2 - p1).Normalised();
-//	auto v2 = (p3 - p1).Normalised();
-//
-//	m_normal = v1.Cross(v2);
-//
-//	m_offset = -p1.Dot(m_normal);
-//}
-//
-//float PlaneCollider::DistanceTo(const Math::Vector3& point) const
-//{
-//	return point.Dot(m_normal) + m_offset;
-//}
-//
-//const Math::Vector3& PlaneCollider::ClosestPoint(const Math::Vector3& point) const
-//{
-//	return point - m_normal * DistanceTo(point);
-//}
-//
-//enum ePlaneResult : int
-//{
-//	FRONT = 1,
-//	BACK = -1,
-//	INTERSECTS = 0
-//};
-//
-//ePlaneResult PlaneCollider::TestSide(const Math::Vector3& point) const
-//{
-//	float t = point.Dot(m_normal) + m_offset;
-//
-//	if (t < 0)
-//	{
-//		return ePlaneResult::BACK;
-//	}
-//	else if (t > 0)
-//	{
-//		return ePlaneResult::FRONT;
-//	}
-//
-//	return ePlaneResult::INTERSECTS;
-//}
-//
-//ePlaneResult PlaneCollider::TestSide(const CircleCollider& circle) const
-//{
-//	float t = DistanceTo(circle.m_center);
-//
-//	if (t > circle.m_radius)
-//	{
-//		return ePlaneResult::FRONT;
-//	}
-//	else if (t < -circle.m_radius)
-//	{
-//		return ePlaneResult::BACK;
-//	}
-//
-//	return ePlaneResult::INTERSECTS;
-//}
-//
-//ePlaneResult PlaneCollider::TestSide(const AABBCollider& aabb) const
-//{
-//	bool side[2] = { false,false };
-//
-//	for (auto c : aabb.Corners())
-//	{
-//		auto result = TestSide(c);
-//		if (result == ePlaneResult::FRONT)
-//		{
-//			side[0] = true;
-//		}
-//		else if (result == ePlaneResult::BACK)
-//		{
-//			side[1] = true;
-//		}
-//	}
-//
-//	if (side[0] && !side[1])
-//	{
-//		return ePlaneResult::FRONT;
-//	}
-//	else if (!side[0] && side[1])
-//	{
-//		return ePlaneResult::BACK;
-//	}
-//
-//	return ePlaneResult::INTERSECTS;
-//}
+#include "PlaneCollider.h"
+#include "CircleCollider.h"
+#include "AABBCollider.h"
+
+PlaneCollider::PlaneCollider(float x, float y, float offset)
+{
+	m_normal = Math::Vector3(x, y, 1);
+	m_offset = offset;
+}
+
+PlaneCollider::PlaneCollider(const Math::Vector3& normal, float offset)
+{
+	m_normal = normal;
+	m_offset = offset;
+}
+
+PlaneCollider::PlaneCollider(const Math::Vector3& p1, const Math::Vector3& p2)
+{
+	auto v = p2 - p1;
+	v.Normalise();
+
+	m_normal.x = -v.y;
+	m_normal.y = v.x;
+
+	m_offset = -p1.Dot(m_normal);
+}
+
+PlaneCollider::PlaneCollider(const Math::Vector3& p1, const Math::Vector3& p2, const Math::Vector3& p3)
+{
+	auto v1 = (p2 - p1).Normalised();
+	auto v2 = (p3 - p1).Normalised();
+
+	m_normal = v1.Cross(v2);
+
+	m_offset = -p1.Dot(m_normal);
+}
+
+float PlaneCollider::DistanceTo(const Math::Vector3& point) const
+{
+	return point.Dot(m_normal) + m_offset;
+}
+
+const Math::Vector3& PlaneCollider::ClosestPoint(const Math::Vector3& point) const
+{
+	return point - m_normal * DistanceTo(point);
+}
+
+PlaneCollider::ePlaneResult PlaneCollider::TestSide(const Math::Vector3& point) const
+{
+	float t = point.Dot(m_normal) + m_offset;
+
+	if (t < 0)
+	{
+		return ePlaneResult::BACK;
+	}
+	else if (t > 0)
+	{
+		return ePlaneResult::FRONT;
+	}
+
+	return ePlaneResult::INTERSECTS;
+}
+
+PlaneCollider::ePlaneResult PlaneCollider::TestSide(const CircleCollider& circle) const
+{
+	float t = DistanceTo(circle.m_center);
+
+	if (t > circle.m_radius)
+	{
+		return ePlaneResult::FRONT;
+	}
+	else if (t < -circle.m_radius)
+	{
+		return ePlaneResult::BACK;
+	}
+
+	return ePlaneResult::INTERSECTS;
+}
+
+PlaneCollider::ePlaneResult PlaneCollider::TestSide(const AABBCollider& aabb) const
+{
+	bool side[2] = { false,false };
+
+	for (auto c : aabb.Corners())
+	{
+		auto result = TestSide(c);
+		if (result == ePlaneResult::FRONT)
+		{
+			side[0] = true;
+		}
+		else if (result == ePlaneResult::BACK)
+		{
+			side[1] = true;
+		}
+	}
+
+	if (side[0] && !side[1])
+	{
+		return ePlaneResult::FRONT;
+	}
+	else if (!side[0] && side[1])
+	{
+		return ePlaneResult::BACK;
+	}
+
+	return ePlaneResult::INTERSECTS;
+}
--- TankGame_Rebuild/PlaneCollider.h
+++ TankGame_Rebuild/PlaneCollider.h
@@ -1,36 +1,41 @@
-//#pragma once
-//#include "Collider.h"
-//#include "Vector3.h"
-//
-//class CircleCollider;
-//class AABBCollider;
-//
-//class PlaneCollider : public Collider
-//{
-//public:
-//	//	VARIABLES
-//
-//	Math::Vector3 m_normal;
-//	float m_offset;
-//
-//	//	CONSTRUCTORS
-//
-//	PlaneCollider() { m_offset = 0.f; }
-//	PlaneCollider(float x, float y, float offset);
-//	PlaneCollider(const Math::Vector3& normal, float offset);
-//	PlaneCollider(const Math::Vector3& p1, const Math::Vector3& p2);
-//	PlaneCollider(const Math::Vector3& p1, const Math::Vector3& p2, const Math::Vector3& p3);
-//
-//	//	METHODS
-//
-//	float DistanceTo(const Math::Vector3& point) const;
-//
-//	const Math::Vector3& ClosestPoint(const Math::Vector3& point) const;
-//
-//	enum ePlaneResult : int;
-//
-//	ePlaneResult TestSide(const Math::Vector3& point) const;
-//	ePlaneResult TestSide(const CircleCollider& circle) const;
-//	ePlaneResult TestSide(const AABBCollider& aabb) const;
-//};
-//
+#pragma once
+#include "Collider.h"
+#include "Vector3.h"
+
+class CircleCollider;
+class AABBCollider;
+
+class PlaneCollider : public Collider
+{
+public:
+	//	VARIABLES
+
+	Math::Vector3 m_normal;
+	float m_offset;
+
+	//	CONSTRUCTORS
+
+	PlaneCollider() { m_offset = 0.f; }
+	PlaneCollider(float x, float y, float offset);
+	PlaneCollider(const Math::Vector3& normal, float offset);
+	PlaneCollider(const Math::Vector3& p1, const Math::Vector3& p2);
+	PlaneCollider(const Math::Vector3& p1, const Math::Vector3& p2, const Math::Vector3& p3);
+
+	//	METHODS
+
+	float DistanceTo(const Math::Vector3& point) const;
+
+	const Math::Vector3& ClosestPoint(const Math::Vector3& point) const;
+
+	enum ePlaneResult : int
+	{
+		FRONT = 1,
+		BACK = -1,
+		INTERSECTS = 0
+	};
+
+	ePlaneResult TestSide(const Math::Vector3& point) const;
+	ePlaneResult TestSide(const CircleCollider& circle) const;
+	ePlaneResult TestSide(const AABBCollider& aabb) const;
+};
+
